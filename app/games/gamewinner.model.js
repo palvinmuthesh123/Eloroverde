c3ffbd9141121de6e59150704352df9c315468c8
Palvin Muthesh <spalvinmuthesh@gmail.com>
2024-02-01 14:05:36 +0530

    ajkfkjafbjkfdjvdkvd vd

--- app/games/gamewinner.model.js
+++ app/games/gamewinner.model.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 const schema = new Schema({
     gameid: { type: String, required: true },
+    uid: { type: String, required: true },
     name: { type: String, required: true },
     email: { type: String, required: true },
     status: { type: String, required: true },
