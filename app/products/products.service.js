027537568a70845fb883bd558920497ec7a18ca8
Palvin Muthesh <spalvinmuthesh@gmail.com>
2024-02-05 19:03:18 +0530

    jkubjnlnklnlknn

--- app/products/products.service.js
+++ app/products/products.service.js
@@ -1004,7 +1004,7 @@ async function Payss(data) {
     // }
     
         var obj = {
-        amount: (parseFloat(data.amount) * 100).toString(), // cents
+        amount: (parseFloat(data.amount)).toString(), // cents
         orderReference: parseInt(Math.random() * 10000000000).toString(),
         merchantName: "INTEGRATION TEST SHOP",
         merchantCode: "327234688",
